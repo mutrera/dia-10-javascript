e5302f4c26ab8b2e061a86c301323241ef991240
Jordi Gasau <50402221+jordigasau@users.noreply.github.com>
2022-11-03 16:12:22 +0100

    exemple de if, else

--- arrays.js
+++ arrays.js
@@ -34,6 +34,7 @@ function demoArrays(){
     numeros.forEach(sumatori)
     console.log("Després del sumatori")
     console.log(total)
+
     function sumatori(valor, index, llista){
         // Mirem que ens està passant
         console.log("Dins del sumatori")
@@ -47,6 +48,83 @@ function demoArrays(){
 
     numeros.forEach(calcularMitjana)
     console.log(mitjana)
+
+    // LOOPS FOR
+
+    console.clear()
+
+
+    var lletres = ["a","b","c","d"]
+    
+    var i = 1
+    for(i; i <100;i = i + 2)
+    {
+        console.log("posició",i)
+    }
+    
+    // posicio++ es igual posicio = posicio + 1
+    for(var posicio=0;posicio<lletres.length;posicio++){
+        console.log(posicio)
+        console.log(lletres[posicio])
+    }
+
+    console.clear()
+
+    function informacio(valor, posicio, arrayOriginal){
+        console.log(posicio)
+        console.log(valor)
+    }
+
+    function senseRes(){
+        console.log('hola')
+    }
+
+    function ambElValor(elValor){
+        console.log(elValor)
+    }
+
+    function ambElValorIlaPosicio(elVal, laPos){
+        console.log(elVal,laPos)
+    }
+    lletres.forEach(informacio)
+    lletres.forEach(senseRes)
+    lletres.forEach(ambElValor)
+    lletres.forEach(ambElValorIlaPosicio)
+
+    lletres.forEach(function(val){
+        console.log(val)
+    })
+
+    var lletres2=[
+        "a",
+        "b",
+        "c"
+    ]
+
+    var excel = [
+        ["Nom","Edat","es soci","telefon"],
+        ["Jordi",42,true,"86846546854"],
+        ["Sandra",34,false,"0928321092"],
+        ["Laura",34,true,"321098321"]
+    ]
+
+    function mostraInfoExcel(fila,pos,original){
+        console.log(original)
+        if(pos == 0){
+            console.log("Estem a la fila 0")
+        }else{
+            console.log("No estem a la fila 0")
+            console.log(fila,pos)
+            console.log("Nom soci:",fila[0])
+            console.log("Edat:",fila[1])
+            console.log(original[0][0],fila[0])
+        }
+    } 
+
+    excel.forEach(mostraInfoExcel)
+
+
+
 }
 
 
